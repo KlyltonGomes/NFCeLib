c16c52e45aa61c34fc997e372a0b05a69c8511c2
KlyltonGomes <klyltongomes@gmail.com>
2025-03-26 15:46:25 -0300

    Iniciando projeto NFCeLib

---
+++ src/main/java/com/klylton/nfcelib/NfCeLibApplication.java
@@ -0,0 +1,13 @@
+package com.klylton.nfcelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfCeLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfCeLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klylton/nfcelib/NfCeLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klylton.nfcelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfCeLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
